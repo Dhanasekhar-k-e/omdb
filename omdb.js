8488a119903a88f50ddf0ca22921f9c563027937
Dhana Sekhar <k.e.dhanasekhar@gmail.com>
2022-08-13 21:32:01 +0530

    xs size added

--- omdb.js
+++ omdb.js
@@ -38,7 +38,7 @@ function search(){
             }
 
             var card = document.createElement('div')
-            card.setAttribute('class','card col-md-4 col-sm-12 col-lg-2')
+            card.setAttribute('class','card col-xs-12 col-md-4 col-sm-6 col-lg-2')
             row.append(card)
            
             var img = document.createElement('img')
